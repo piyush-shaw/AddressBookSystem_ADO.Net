88e76971f1287f84b8b3ebbcfa7ed6729d68b369
piyush-shaw <piyushshaw0000@gmail.com>
2023-02-24 13:19:53 +0530

    [Piyush] added to create addressbook database using Ado.net

---
+++ AddressBookSystem/AddressBookSystem/AddressRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AddressBookSystem
+{
+	public class AddressRepo
+	{
+        public static string connectionString = "Server=127.0.0.1,1433;Database=Payroll;User Id=sa;Password=Valuetech@123";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
+
